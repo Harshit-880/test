8e801fd6eb2bf52f7e942d19cd8ce516b7f91921
harshit garg <gargankit4629@gmail.com>
2023-05-02 15:21:17 +0530

    first

--- todolist/mainapp/urls.py
+++ todolist/mainapp/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('<int:pk>/', DetailToDo.as_view()),
     path('',ListToDo.as_view()),
     path('create', CreateToDo.as_view()),   
-    path('delete/<int:pk>/', DeleteToDo.as_view()),
+    path('delete/<int:pk>/', DeleteToDo.as_view())
 ]
